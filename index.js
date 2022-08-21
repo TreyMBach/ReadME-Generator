81b97b71519db3783697eabf320ef5f08038d51a
Trey Bach <treybachyt@gmail.com>
2022-08-21 13:48:14 -0700

    Updated Index.js to generate Readme files based off  terminal input

--- index.js
+++ index.js
@@ -2,56 +2,68 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 
-const generateREADME = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+const generateREADME = ({ projectTitle, projectDescription, installationRequirements, usage, license, github, linkedin }) =>
+  `# ${projectTitle}
+  
+  ## Project Description:
+
+    ${projectDescription}
+
+    - [Installation](##installation)
+    - [Usage](##usage)
+    - [Credits](##credits)
+    - [License](##license)
+
+    ## Installation
+
+    ${installationRequirements}
+
+    ## Usage
+
+    ${usage}
+
+    ##License
+
+    The license we used is ${license}.
+    
+    ## Links
+
+    My github link github.com/${github}.
+    My LinkedIN is ${linkedin}
+
+  `;
 
 inquirer
 .prompt([
   {
     type: 'input',
-    name: 'name',
-    message: 'What is your name?',
+    name: 'projectTitle',
+    message: 'What is the Project Title',
   },
   {
     type: 'input',
-    name: 'location',
-    message: 'Where are you from?',
+    name: 'projectDescription',
+    message: 'What is a short description of the Project?',
   },
   {
     type: 'input',
-    name: 'hobby',
-    message: 'What is your favorite hobby?',
+    name: 'installationRequirements',
+    message: 'What are the installation requirements for this Project?',
   },
   {
     type: 'input',
-    name: 'food',
-    message: 'What is your favorite food?',
+    name: 'usage',
+    message: 'How would you use this application?',
   },
   {
     type: 'input',
     name: 'github',
     message: 'Enter your GitHub Username',
+  },
+  {
+    name: 'license',
+    type: 'list',
+    choices: ['Apache', 'BSD 3', 'IBM', 'MIT']
   },
   {
     type: 'input',
@@ -62,7 +74,7 @@ inquirer
 .then((answers) => {
   const readmeContent = generateREADME(answers);
 
-  fs.writeFile('README.md', readmeContent, (err) =>
+  fs.writeFile('savedREADME.md', readmeContent, (err) =>
     err ? console.log(err) : console.log('Successfully created the README File!')
   );
 });
